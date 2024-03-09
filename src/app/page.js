05073b48ebf1f7134dd519d35f6e8a7f9f745fb6
dev-rabin <robinmandhotia@gmail.com>
2024-03-09 21:31:13 +0530

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,79 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import React from "react";
+import "./page.css";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMultiply,
+  faStar,
+  faThumbsDown,
+  faThumbsUp,
+} from "@fortawesome/free-solid-svg-icons";
 
-export default function Home() {
+function HomePage() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
+    <>
+      <div className="container">
+        <FontAwesomeIcon icon={faMultiply} size="2x" />
+        <h1>Leave a review</h1>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <h3>Safety</h3>
+          <p>Lorem ipsum dolor sit, amet consectetur</p>
+          <div className="safety-div">
+            <FontAwesomeIcon icon={faStar} size="3x" color="yellow" />{" "}
+            <span>
+              <FontAwesomeIcon icon={faStar} size="3x" color="yellow" />
+            </span>
+            <span>
+              <FontAwesomeIcon icon={faStar} size="3x" color="yellow" />
+            </span>
+            <span>
+              <FontAwesomeIcon icon={faStar} size="3x" color="yellow" />
+            </span>
+            <span>
+              <FontAwesomeIcon icon={faStar} size="3x" color="yellow" />
+            </span>
+          </div>
+        </div>
+        <div className="communication-div">
+          <h3>Communication</h3>
+          <p>Lorem ipsum dolor sit, amet consectetur</p>
+          <FontAwesomeIcon icon={faStar} size="3x" color="yellow" />{" "}
+          <span>
+            <FontAwesomeIcon icon={faStar} size="3x" color="yellow" />
+          </span>
+          <span>
+            <FontAwesomeIcon icon={faStar} size="3x" color="yellow" />
+          </span>
+          <span>
+            <FontAwesomeIcon icon={faStar} size="3x" color="yellow" />
+          </span>
+          <span>
+            <FontAwesomeIcon icon={faStar} size="3x" color="grey" />
+          </span>
+        </div>
+        <div className="recommend-div">
+          <h3>Would you recommend Trausti?</h3>
+          <p>Lorem ipsum dolor sit, amet consectetur</p>
+         <div className="thumb-icons">
+         <span >
+            <FontAwesomeIcon icon={faThumbsDown} size="2x" color="grey" className="thumb-span-icons" />
+           <span>No</span>
+          </span>
+          <span className="thumb-span-icons">
+            <FontAwesomeIcon icon={faThumbsUp} size="2x" color="green" className="thumb-span-icons" />
+            <span>Yes</span>
+          </span>
+         </div>
+        </div>
+        <div>
+          <h3>Praise</h3>
+          <p>Lorem ipsum dolor sit amet.</p>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
+
+export default HomePage;
